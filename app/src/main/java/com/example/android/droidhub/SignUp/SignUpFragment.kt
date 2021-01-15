11c444f2ae63c5fdeaebde7e165345f2b3593cb8
voiced <opeyemi.k05@gamil.com>
2021-01-15 13:31:54 +0100

    authentication activated

--- app/src/main/java/com/example/android/droidhub/SignUp/SignUpFragment.kt
+++ app/src/main/java/com/example/android/droidhub/SignUp/SignUpFragment.kt
@@ -2,76 +2,128 @@ package com.example.android.droidhub.SignUp
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.android.droidhub.R
 import com.example.android.droidhub.databinding.FragmentSignUpBinding
-import com.example.android.droidhub.utilities.verifyEmail
-import com.example.android.droidhub.utilities.verifyName
-import com.example.android.droidhub.utilities.verifyPassword
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUpFragment : Fragment() {
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
+    private lateinit var name: TextInputEditText
+    private lateinit var email: TextInputEditText
+    private  lateinit var password: TextInputEditText
     private lateinit var auth: FirebaseAuth
     private lateinit var load:ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentSignUpBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        load = binding.load
+        name = binding.nameEditText
+        email = binding.emailEditText
+        password = binding.passwordEditText
+        load.visibility = View.INVISIBLE
         auth = FirebaseAuth.getInstance()
 
         binding.orLogin.setOnClickListener {
             view.findNavController().navigate(R.id.action_signUpFragment_to_logInFragment)
         }
 
-        load = binding.load
         binding.signUpButton.setOnClickListener {
-            load.visibility = View.VISIBLE
-
-            if (verifyName(this@SignUpFragment, binding.nameEditText.text.toString()) &&
-                    verifyEmail(this@SignUpFragment, binding.emailEditText.text.toString()) &&
-                    verifyPassword(this@SignUpFragment, binding.passwordEditText.text.toString()))
-            {
-
-            }
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user = auth.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-
-                    // ...
-                }
+            signUpUser()
         }
     }
 
+    private fun  signUpUser() {
+        load.visibility = View.VISIBLE
+        if (name.text.toString().isEmpty()) {
+            name.error = "Please provide your name"
+            name.requestFocus()
+            return
+        }
+        if (name.length() < 4) {
+            name.error = "Name is too short"
+            name.requestFocus()
+            return
+        }
+        if (email.text.toString().isEmpty()) {
+            email.error = "Please provide your email"
+            email.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+            email.error = "Please enter a valid email"
+            email.requestFocus()
+            return
+        }
+
+        if (password.text.toString().isEmpty()) {
+            password.error = "Please enter a password"
+            password.requestFocus()
+            return
+        }
+
+        if (password.length() < 6) {
+            password.error = "Password is too short"
+            password.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    user!!.sendEmailVerification()
+                        .addOnCompleteListener { task ->
+                            load.visibility = View.INVISIBLE
+                            if (task.isSuccessful) {
+                                view?.findNavController()?.navigate(R.id.action_signUpFragment_to_filesFragment)
+                            }
+                        }
+                    Toast.makeText(
+                        context, "User Sign up successful", Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        context, "User Sign up failed. Try again in a few moment",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser != null) {
+            Log.d("UID  ", auth.currentUser!!.uid)
+            Navigation.findNavController(requireView())
+                .navigate(R.id.action_signUpFragment_to_filesFragment)
+        }
+    }
+
+
     override fun onDestroyView() {
         super.onDestroyView()
-
         // Set the binding to null when fragment views are destroyed in order to avoid memory leaks
         _binding = null
     }
 }
+
--- app/src/main/java/com/example/android/droidhub/main/LogInFragment.kt
+++ app/src/main/java/com/example/android/droidhub/main/LogInFragment.kt
@@ -1,47 +1,98 @@
 package com.example.android.droidhub.main
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.droidhub.R
 import com.example.android.droidhub.databinding.FragmentLogInBinding
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class LogInFragment : Fragment() {
     private var _binding: FragmentLogInBinding? = null
     private val binding get() = _binding!!
+    private lateinit var email: TextInputEditText
+    private  lateinit var password: TextInputEditText
     private lateinit var auth: FirebaseAuth
     private lateinit var load: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentLogInBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        load = binding.load
+        email = binding.emailEditText
+        password = binding.passwordEditText
+        load.visibility = View.INVISIBLE
         auth = FirebaseAuth.getInstance()
 
         binding.orSignUp.setOnClickListener {
             view.findNavController().navigate(R.id.action_logInFragment_to_signUpFragment)
         }
 
-        load = binding.load
         binding.logInButton.setOnClickListener {
-            load.visibility = View.VISIBLE
+            signInUser()
         }
 
     }
 
-    public override fun onStart() {
+    private fun signInUser() {
+        load.visibility = View.VISIBLE
+
+        if (email.text.toString().isEmpty()) {
+            email.error = "Please provide your email"
+            email.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+            email.error = "Please enter a valid email"
+            email.requestFocus()
+            return
+        }
+
+        if (password.text.toString().isEmpty()) {
+            password.error = "Please enter a password"
+            password.requestFocus()
+            return
+        }
+
+        if (password.length() < 6) {
+            password.error = "Password is too short"
+            password.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    load.visibility = View.GONE
+                    Toast.makeText(
+                        context, "User successfully signed in", Toast.LENGTH_SHORT
+                    ).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    load.visibility = View.INVISIBLE
+                    updateUI(null)
+                }
+            }
+    }
+
+    override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
@@ -49,7 +100,21 @@ class LogInFragment : Fragment() {
     }
 
     private fun updateUI(currentUser: FirebaseUser?) {
-
+        if (currentUser != null) {
+            if (currentUser.isEmailVerified) {
+                view?.findNavController()?.navigate(R.id.action_logInFragment_to_filesFragment)
+            } else {
+                Toast.makeText(
+                    context, "Please verify your email address",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                context, "User sign In failed. Check user details",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onDestroyView() {
